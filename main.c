525217ab6a4e93fa86dfbe43881627df981982ae
Samuel Tesla <samuel.tesla@gmail.com>
2010-09-15 15:08:22 -0700

    add stream mode in addition to REPL

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include "object.h"
 #include "read.h"
 #include "print.h"
 
 int main(int argc, const char **argv) {
-  for (;;) {
-    printf("> ");
-    ref_t expr = readsexp(stdin);
+  ref_t expr;
+  if (argc == 2 && !strcmp("-", argv[1])) {
+    /* read(stdin) -> print(stdout) */
+    expr = readstream(stdin);
     print(expr);
     puts("");
+  } else {
+    /* REPL */
+    for (;;) {
+      printf("> ");
+      expr = readsexp(stdin);
+      print(expr);
+      puts("");
+    }
   }
   return 0;
 }
