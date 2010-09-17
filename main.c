79db72a9cb7058bfada9d75f7863a3e56ee437ec
Samuel Tesla <samuel.tesla@gmail.com>
2010-09-16 22:10:09 -0700

    added do mode for evaluating code

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 #include "env.h"
 #include "error.h"
 #include "eval.h"
@@ -8,13 +9,9 @@
 #include "read.h"
 #include "print.h"
 
-static void compile(env_t *env) {
-  if (setjmp(error_loc) == 0) {
-    print(readstream(env, stdin));
-    puts("");
-  } else {
-    fprintf(stderr, "ERROR: %s\n", the_error);
-  }
+static usage() {
+  /* TODO */
+  exit(1);
 }
 
 static void repl(env_t *env) {
@@ -28,12 +25,48 @@ static void repl(env_t *env) {
   }
 }
 
-int main(int argc, const char **argv) {
+static void do_it(env_t *env, const char *filename) {
+  FILE *input = !strcmp("-", filename) ? stdin : fopen(filename, "r");
+  if (setjmp(error_loc) == 0) {
+      ref_t program = readstream(env, input);
+      ref_t result;
+      do {
+        result = eval(car(program));
+        program = cdr(program);
+      } while (!isnil(program));
+      print(result);
+      puts("");
+  } else {
+    fprintf(stderr, "ERROR: %s", the_error);
+    exit(1);
+  }
+}
+
+int main(int argc, char **argv) {
+  int ch;
+  bool do_mode = NO;
+  const char *input_file;
+
+  static struct option longopts[] = {
+    {"do", optional_argument, NULL, 'd'}
+  };
+  while ((ch = getopt_long(argc, argv, "d:-", longopts, NULL)) != -1) {
+    switch(ch) {
+    case 'd':
+      do_mode = YES;
+      input_file = optarg;
+      break;
+    default:
+      usage();
+    }
+  }
+
   env_t *env = make_env();
-  if (argc == 2 && !strcmp("-", argv[1]))
-    compile(env);
+  if (do_mode)
+    do_it(env, input_file);
   else
     repl(env);
   free(env);
+
   return 0;
 }
