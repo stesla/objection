cb6ee4b5f7c285241a780d978bf8f1920de511c0
Samuel Tesla <samuel.tesla@gmail.com>
2010-09-17 21:23:48 -0700

    move options inside $program
    If $program is going to be a variable that can be set, then it should
    include the command line options. Not every program will use the same
    options.

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 for test in ${@:-*.test}; do
     printf "%s..." $(basename -s .test $test)
     expected=$(sed -e '1,/RESULT/d' -e '/^$/d'< $test)
-    actual=$(sed -e '/RESULT/,$d' -e '/^$/d' < $test | ${program:=$PWD/object} -d - 2>&1)
+    actual=$(sed -e '/RESULT/,$d' -e '/^$/d' < $test | ${program:=$PWD/object -d -} 2>&1)
     if [ $? -ne 0 ]; then
         status=1
         echo ERROR
