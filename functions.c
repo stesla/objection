68f5cf53128e1d83ecf44b29e3dbb336b0af3fae
Samuel Tesla <samuel.tesla@gmail.com>
2010-09-19 23:18:04 -0700

    make math functions strictly binary

--- functions.c
+++ functions.c
@@ -8,38 +8,25 @@ static ref_t check_integer(ref_t obj) {
   return obj;
 }
 
-static ref_t apply_integer_op(ref_t (*op)(ref_t, ref_t), ref_t acc, ref_t args) {
-  while (!isnil(args)) {
-    ref_t thecar = check_integer(car(args));
-    acc = op(acc, thecar);
-    args = cdr(args);
-  }
-  return acc;
+static ref_t binary_integer_op(ref_t (*op)(ref_t, ref_t), ref_t args) {
+  ref_t x = check_integer(car(args)), y = check_integer(cadr(args));
+  return op(x, y);
 }
 
 static ref_t fn_add(ref_t args) {
-  return apply_integer_op(integer_add, integer(0), args);
+  return binary_integer_op(integer_add, args);
 }
 
 static ref_t fn_sub(ref_t args) {
-  ref_t thecar = car(args), thecdr = cdr(args);
-  if (isnil(thecar))
-    return 0;
-  else if (isnil(thecdr))
-    return integer_sub(0, check_integer(thecar));
-  else
-    return apply_integer_op(integer_sub, check_integer(thecar), thecdr);
+  return binary_integer_op(integer_sub, args);
 }
 
 static ref_t fn_mul(ref_t args) {
-  return apply_integer_op(integer_mul, integer(1), args);
+  return binary_integer_op(integer_mul, args);
 }
 
 static ref_t fn_div(ref_t args) {
-  ref_t thecar = check_integer(car(args));
-  ref_t thecadr = check_integer(cadr(args));
-  int result = integer_div(thecar, thecadr);
-  return apply_integer_op(integer_div, result, cddr(args));
+  return binary_integer_op(integer_div, args);
 }
 
 static ref_t fn_eq(ref_t args) {
@@ -50,15 +37,15 @@ static ref_t fn_list(ref_t args) {
   return args;
 }
 
-static inline make_builtin(env_t *env, const char *name, fn_t impl, size_t arity, bool rest) {
+static inline void make_builtin(env_t *env, const char *name, fn_t impl, size_t arity, bool rest) {
   set_function(intern(env, name), function(impl, arity, rest));
 }
 
 void init_builtin_functions(env_t *env) {
-  make_builtin(env, "+", fn_add, 0, YES);
-  make_builtin(env, "-", fn_sub, 0, YES);
-  make_builtin(env, "*", fn_mul, 0, YES);
-  make_builtin(env, "/", fn_div, 2, YES);
+  make_builtin(env, "+", fn_add, 2, NO);
+  make_builtin(env, "-", fn_sub, 2, NO);
+  make_builtin(env, "*", fn_mul, 2, NO);
+  make_builtin(env, "/", fn_div, 2, NO);
   make_builtin(env, "eq", fn_eq, 2, NO);
   make_builtin(env, "list", fn_list, 0, YES);
 }
