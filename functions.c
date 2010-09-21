815983be12db3e0cd3da1eefed38d6907063da39
Samuel Tesla <samuel.tesla@gmail.com>
2010-09-21 00:20:46 -0700

    added FSET builtin function

--- functions.c
+++ functions.c
@@ -2,11 +2,23 @@
 #include "functions.h"
 #include "object.h"
 
-static ref_t check_integer(ref_t obj) {
-  if (!isinteger(obj))
-    /* TODO: would be good to have a repr type function */
-    error("not an integer");
+/* TODO: would be good to have a repr type function */
+static ref_t check(bool (*test)(ref_t), const char *msg, ref_t obj) {
+  if (!test(obj))
+    error(msg);
   return obj;
+}
+
+static ref_t check_integer(ref_t obj) {
+  return check(isinteger, "not an integer", obj);
+}
+
+static ref_t check_function(ref_t obj) {
+  return check(isfunction, "not a function", obj);
+}
+
+static ref_t check_symbol(ref_t obj) {
+  return check(issymbol, "not a symbol", obj);
 }
 
 static ref_t binary_integer_op(ref_t (*op)(ref_t, ref_t), ref_t args) {
@@ -38,6 +50,12 @@ static ref_t fn_list(env_t *env, ref_t func, ref_t args) {
   return args;
 }
 
+static ref_t fn_fset(env_t *env, ref_t func, ref_t args) {
+  ref_t symbol = check_symbol(car(args)), fn = check_function(cadr(args));
+  set_function(symbol, fn);
+  return fn;
+}
+
 static inline void make_builtin(env_t *env, const char *name, fn_t impl, size_t arity, bool rest) {
   set_function(intern(env, name), function(impl, NIL, arity, rest));
 }
@@ -48,5 +66,6 @@ void init_builtin_functions(env_t *env) {
   make_builtin(env, "*", fn_mul, 2, NO);
   make_builtin(env, "/", fn_div, 2, NO);
   make_builtin(env, "eq", fn_eq, 2, NO);
+  make_builtin(env, "fset", fn_fset, 2, NO);
   make_builtin(env, "list", fn_list, 0, YES);
 }
