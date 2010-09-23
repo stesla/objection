5105c9287be1412ddfbfcadc3198c7bc59d72d76
Samuel Tesla <samuel.tesla@gmail.com>
2010-09-23 16:19:44 -0700

    fix test.sh script
    It appears I was using a MacOSX or BSD specific incantation of
    basename. Fixed it to use the POSIX syntax.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for test in ${@:-*.test}; do
-    printf "%s..." $(basename -s .test $test)
+    printf "%s..." $(basename $test .test)
     expected=$(sed -e '1,/RESULT/d' -e '/^$/d'< $test)
     actual=$(sed -e '/RESULT/,$d' -e '/^$/d' < $test | ${program:=$PWD/object -d -} 2>&1)
     if [ $? -ne 0 ]; then
